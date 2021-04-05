6946b93901a5fe7df65bb49204280d89d290fa48
JOSE HERNAN RIOS PINEDO <hernanriospinedo@gmail.com>
2021-04-04 20:04:09 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,22 @@
 const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Explosion"));
+bands.addBand(new Band("Marilia"));
+bands.addBand(new Band("Proyecto S"));
+
+console.log(bands);
+
 // Mensajes de Sockets
 io.on("connection", client => {
   console.log("cliente conectado");
+
+  client.emit("active-bands", bands.getBands());
+
   client.on("disconnet", () => {
     console.log("cliente desconectado");
   });
@@ -10,4 +25,28 @@ io.on("connection", client => {
     console.log("Mensaje", payload);
     io.emit("mensaje", { admin: "Nuevo mensaje" });
   });
+
+  client.on("vote-band", payload => {
+
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+
+  });
+
+  client.on("add-band", payload => {
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit("active-bands", bands.getBands());
+    
+  });
+
+
+
+
+
+  /*client.on("emitir-mensaje", payload => {
+    //console.log(payload);
+    //io.emit('nuevo-mensaje', payload); emite a todos
+    client.broadcast.emit("nuevo-mensaje", payload); // emite a todos menos el que lo emitio
+  });*/
 });
